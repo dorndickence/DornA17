87e548ba65f5219e3564fcbca2f51f8cc5ee09f1
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-05-01 21:54:48 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+//
+global.available = true;
+global.autoReadAll = true;
+global.antitags = true;
+//auto functione
+global.autoTyping = false;                //auto tyingbydefaul off.
+global.autoRecord = false;                //auto recording bydefault off.
+global.groupevent = true;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
+global.autoreadgc = true;
+//
+global.prefa = ["!"];                                    //Default prefix here. you can change if you want.
+//
+global.Owner = ["‪‪254113907065"];      //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["254113907065‬"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["254113907065‬"];
+global.OwnerName = "Dorn Dickence";
+global.BotName = "Walking chemical";
+global.packname = "DornBot";                             //Do not change.
+global.author = "By: Dorn and Kimberly";                               //Do not change.
+global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
+global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+
+//
+global.sessionName = "session";                          //Do not change.
+
+//
+global.openAiAPI = "sk-C8AfTSAQ4FLMgC594lbnT3BlbkFJdKDQFcvFXuvMQ9w75mjVV";
+
+//
+global.location = "Nairobi, Kenya";
+global.reactmoji = "❤️";
+global.themeemoji = "💖";
+global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
+global.websitex = "https://github.com/Kai0071";
+global.lolhuman = "DornsaS";
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//
+global.mess = {
+  jobdone: 'Here you go...',
+  useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+  botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+  botowner: 'Only my *Owner* can use this command, Baka!',
+  grouponly: 'This command is only made for *Groups*, Baka!',
+  privateonly: 'This command is only made for *Private Chat*, Baka!',
+  botonly: 'Only the *Bot itself* can use this command!',
+  waiting: 'Just Wait...',
+  nolink: 'Please provide me *link*, Baka!',
+  error: 'An error occurd!',
+  banned: 'You are *Banned* fron using commands!',
+  bangc: 'This Group is *Banned* from using Commands!',
+  nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
